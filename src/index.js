54c2795e7f765f7c375941bbd5e7a989eab2bcc3
Suyash <suyash.krishna9@gmail>
2021-04-08 21:15:56 +0400

    Add reducers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './assets/css/index.scss';
 import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import reducer from './reducers/index';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
