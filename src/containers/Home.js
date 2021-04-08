aec1fb2a13156df4f1670fc1b7899015d9440eaf
Suyash <suyash.krishna9@gmail>
2021-04-08 21:20:19 +0400

    Load pets and filters on Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { useSelector } from 'react-redux';
+import Pet from '../components/Pet';
+import BreedFilter from '../components/BreedFilter';
+import '../assets/css/Home.scss';
 
-const Home = () => (
-  <div>Home</div>
-);
+const Home = () => {
+  const [list, setList] = useState([]);
+  const [category, setCategory] = useState('All');
+  const [loadCount, setLoadCount] = useState(0);
+  const pets = useSelector((state) => state.pets);
+  const showList = () => {
+    setLoadCount(loadCount + 1);
+    return _.slice(pets, (loadCount * 9), (((loadCount + 1) * 9) - 1));
+  };
+
+  useEffect(() => {
+    if (category === 'All') {
+      setList(_.slice(pets, ((loadCount - 1) * 9), ((loadCount * 9) - 1)));
+    } else {
+      setList(_.filter(pets, (pet) => pet.breed.name === category));
+    }
+  }, [pets, category]);
+
+  return (
+    <>
+      <BreedFilter category={category} setCategory={setCategory} />
+      <div className="row wrap">
+        {list.map((pet) => (
+          <Pet key={pet.breed.id + pet.pet.id} pet={pet} />
+        ))}
+      </div>
+      {category === 'All'
+        ? <button type="button" onClick={() => { setList(_.concat(list, showList())); }} className="show-more link">Show more</button>
+        : <div />}
+    </>
+  );
+};
 
 export default Home;
