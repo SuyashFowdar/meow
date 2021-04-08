374ad782c29176ea58dfe3fa5fcb60af77fdbb16
Suyash <suyash.krishna9@gmail>
2021-04-08 21:17:39 +0400

    Add background and breed name to pet

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import '../assets/css/Pet.scss';
 
-const Pet = () => (
-  <div>Pet</div>
+const Pet = ({ pet }) => (
+  <Link to={`/pet/${pet.pet.id}`} className="pet-item bg-cover col main-end" style={{ backgroundImage: `url(${pet.pet.url})` }}>
+    <div className="breed">{pet.breed.name}</div>
+  </Link>
 );
+
+Pet.defaultProps = {
+  pet: {},
+};
+
+Pet.propTypes = {
+  pet: PropTypes.instanceOf(Object),
+};
 
 export default Pet;
